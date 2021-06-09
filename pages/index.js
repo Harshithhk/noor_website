9f96d735eaf2437504ed63068477d21128201c3e
Harshithhk <60171634+Harshithhk@users.noreply.github.com>
2021-06-09 21:38:16 +0530

    nav

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,10 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        
-      </main>
-
+      <main className={styles.main}></main>
     </div>
-  )
+  );
 }
