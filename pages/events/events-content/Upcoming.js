6a73b45bc03c4e7bb03cabc30e71c4b02297a11b
Harshithhk <60171634+Harshithhk@users.noreply.github.com>
2021-06-12 13:21:02 +0530

    cards design

--- pages/events/events-content/Upcoming.js
+++ pages/events/events-content/Upcoming.js
@@ -1,13 +1,13 @@
 import styles from "./events_content.module.css";
 import UpcomingCard from "../../../components/events-cards/upcoming-card/UpcomingCard";
+import { upcomingEventsData } from "../../../utils/dummy-data/upcomingEventsData";
 const Upcoming = () => {
-  let noOfCards = ["#FCD5E4", "#AAF1DB", "#C1E1FF", "#C1E1FF", "#E8C7FF"];
   return (
     // <div>hello from upcoming</div>
 
     <div className={styles.events_cards_container}>
-      {noOfCards.map((element) => {
-        return <UpcomingCard key={element} bgColor={element} />;
+      {upcomingEventsData.map((element) => {
+        return <UpcomingCard key={element.id} bgColor={element.bgColor} bgUrl={element.bgUrl} />;
       })}
     </div>
   );
