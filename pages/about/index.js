10cede5ae90f928c70fd27d1070e2ad2ae7b9a09
Harshithhk <60171634+Harshithhk@users.noreply.github.com>
2021-07-13 23:39:43 +0530

    Additions: About Us till Meet the team

---
+++ pages/about/index.js
@@ -0,0 +1,136 @@
+import React from "react"
+import styles from "./about_us.module.css"
+import ResponsivePlayer from "../../components/video-player/ResponsivePlayer"
+const index = () => {
+  return (
+    <section className={styles.about_us_page_wrapper}>
+      <main>
+        <div className={styles.heading}>
+          <div style={{ textAlign: "center", alignItems: "center" }}>About Us</div>
+        </div>
+        <div className={styles.video}>
+          <img src="/assets/images/video-placehholder.png" alt="" />
+        </div>
+        <section>
+          <div className={styles.heading}>
+            <div style={{ textAlign: "center", alignItems: "center" }}>Founder's message</div>
+          </div>
+          <div className={styles.founders_message}>
+            <div className={styles.message}>
+              <div className={styles.message_heading}>Hello there!</div>
+              <p className={styles.message_description}>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                <br /> <br /> Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+                amet.{" "}
+                <div style={{ marginTop: "0.70rem" }}>
+                  {" "}
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                  erat, sed diam
+                </div>
+              </p>
+            </div>
+            <div className={styles.message_img}>
+              <img src="/assets/images/Group.png" alt="" />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className={styles.heading}>
+            <div style={{ textAlign: "center", alignItems: "center" }}>Meet the founders</div>
+          </div>
+          <div className={styles.person}>
+            <div className={styles.person_image} style={{ paddingRight: " 5rem" }}>
+              <img src="/assets/images/Capture.png" alt="" />
+            </div>
+
+            <div className={styles.person_details}>
+              <div>
+                <div className={styles.name}>Zeel</div>
+                <div className={styles.designation}>Designation</div>
+                <div className={styles.text}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                  erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                  est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.person}>
+            <div className={styles.person_details}>
+              <div>
+                <div className={styles.name}>Mugdha</div>
+                <div className={styles.designation}>Designation</div>
+                <div className={styles.text}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                  erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                  est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.
+                </div>
+              </div>
+            </div>
+            <div className={styles.person_image} style={{ paddingLeft: " 5rem" }}>
+              <img src="/assets/images/Capture1.png" alt="" />
+            </div>
+          </div>
+          <div className={styles.person}>
+            <div className={styles.person_image} style={{ paddingRight: " 5rem" }}>
+              <img src="/assets/images/Capture2.png" alt="" />
+            </div>
+
+            <div className={styles.person_details}>
+              <div>
+                <div className={styles.name}>Piyush</div>
+                <div className={styles.designation}>Designation</div>
+                <div className={styles.text}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                  erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                  est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* <ResponsivePlayer /> */}
+        <section>
+          <div className={styles.heading}>
+            <div style={{ textAlign: "center", alignItems: "center" }}>Meet the team</div>
+          </div>
+          <div className={styles.meet_the_team_avatars}>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <div className={styles.avtars_container}>
+                <img src="/assets/images/Meet-the-team/Avatar1.svg" alt="" />
+                <div className={styles.avatar_name}> Manas </div>
+              </div>
+              <div className={styles.avtars_container}>
+                <img src="/assets/images/Meet-the-team/Avatar2.svg" alt="" />
+                <div className={styles.avatar_name}> Manasvi </div>
+              </div>
+              <div className={styles.avtars_container}>
+                <img src="/assets/images/Meet-the-team/Avatar3.svg" alt="" />
+                <div className={styles.avatar_name}> Tejaswini </div>
+              </div>
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-around", marginTop: "3rem" }}>
+              <div className={styles.avtars_container}>
+                <img src="/assets/images/Meet-the-team/Avatar4.svg" alt="" />
+                <div className={styles.avatar_name}> Ramesh </div>
+              </div>
+              <div className={styles.avtars_container}>
+                <img src="/assets/images/Meet-the-team/Avatar5.svg" alt="" />
+                <div className={styles.avatar_name}> Ashish </div>
+              </div>
+              <div className={styles.avtars_container}>
+                <img src="/assets/images/Meet-the-team/Avatar6.svg" alt="" />
+                <div className={styles.avatar_name}> Jenna </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </section>
+  )
+}
+
+export default index
